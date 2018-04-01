1ba0cb654f4261bca466ceec5dbd593c431830e0
hitheima <hitheima@163.com>
2018-04-01 19:34:44 +0800

    1-新建aaa

--- scripts/test_file.py
+++ scripts/test_file.py
@@ -17,6 +17,8 @@ class TestFile:
         self.file_page.create_dir_with_name("zzz")
 
         # 新建文件夹aaa
+        self.file_page.create_dir_with_name("aaa")
+        
         # 进入zzz
         # 创建1-20.txt文件
         # 移动到aaa目录中
